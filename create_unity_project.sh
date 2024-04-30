bdf7d0ef766b668b15bf48e26d4e7226390400cb
alexlagorce <lagorce.alexandre@gmail.com>
2024-04-30 11:17:11 +0200

    change name project

--- create_unity_project.sh
+++ create_unity_project.sh
@@ -2,7 +2,7 @@
 
 # Creation d'un nouveau projet Unity
 
-~/Unity-2021.3.33f1/Editor/Unity  -createProject CohomaProject
+~/Unity-2021.3.33f1/Editor/Unity  -createProject UnityProject
 
 # Suppression des dossiers Assets et Packages du nouveau projet
 
